7bc5702e0284113d73c47f311f06ce9bc59347fc
Luis <luis.diolindo@gmail.com>
2019-08-13 15:52:55 +0300

    jsp added and servlets

--- src/com/octest/servlets/Test.java
+++ src/com/octest/servlets/Test.java
@@ -1,6 +1,7 @@
 package com.octest.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,19 +16,17 @@ public class Test extends HttpServlet {
 
     public Test() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/hello.jsp").forward(request, response);
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
